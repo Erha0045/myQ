b6f7ba6bb6f2ae6500e270d8abd21017fe238955
erhbas <e.basaran.111@gmail.com>
2022-05-12 14:07:46 +0200

    Collected two controllers to one, 3 tabels is showed in same html index

--- src/main/java/com/hovedopgave/myq/Controllers/indexController.java
+++ src/main/java/com/hovedopgave/myq/Controllers/indexController.java
@@ -1,10 +1,12 @@
 package com.hovedopgave.myq.Controllers;
 
 import com.hovedopgave.myq.Entities.Parameter;
+import com.hovedopgave.myq.Entities.QueTask;
 import com.hovedopgave.myq.Entities.TimeResolution;
 import com.hovedopgave.myq.Repositories.ParameterRepository;
 import com.hovedopgave.myq.Repositories.TimeResolutionRepository;
 import com.hovedopgave.myq.Services.ParameterService;
+import com.hovedopgave.myq.Services.QueService;
 import com.hovedopgave.myq.Services.TimeResolutionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -24,6 +26,9 @@ public class indexController {
     @Autowired
     TimeResolutionService timeResolutionService;
 
+    @Autowired
+    QueService queService;
+
 
     @GetMapping("/")
     public String showHomePage(Model model){
@@ -32,6 +37,9 @@ public class indexController {
 
             List<TimeResolution> timeList = timeResolutionService.findAll();
             model.addAttribute("showTime", timeList);
+
+        List<QueTask> queTaskList = queService.listAll();
+        model.addAttribute("qList", queTaskList);
 
         return "index";
 
